07738a27adc767e2acd21b37a09d37d50187a022
wboka <boka.wayne+GitHub@gmail.com>
2021-04-29 21:30:13 -0400

    feat: add a better menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 const Menu = () => {
@@ -6,28 +5,57 @@ const Menu = () => {
 		<div className="menu">
 			<div className="container">
 				<div className="row">
-					<div
-						className="col-4 col-sm-2"
-						style={{ justifyContent: "center", alignItems: "center" }}
-					>
-						<Image
-							src="/weather.png"
-							alt="Orange sun, grey clouds, grey rain, and yellow lightning bolt"
-							style={{ maxWidth: "50px", width: "100%" }}
-							width={50}
-							height={50}
-						/>
-					</div>
-					<div className="col-8 title" style={{ textAlign: "center" }}>
+					<div className="col-2 home">
 						<Link href="/">
 							<a>
-								<h1>Just the Weather!</h1>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="icon"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={1}
+										d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+									/>
+								</svg>
 							</a>
 						</Link>
 					</div>
-					<div className="col-12 col-sm-2 settings">
+					<div className="col-8 title" style={{ textAlign: "center" }}>
+						<img
+							src="/weather.png"
+							alt="Orange sun, grey clouds, grey rain, and yellow lightning bolt"
+							style={{
+								maxWidth: "35px",
+								width: "100%",
+								margin: "8px 8px 0 auto",
+							}}
+							width={35}
+						/>
+						Just the Weather!
+					</div>
+					<div className="col-2 settings">
 						<Link href="/settings">
-							<a>Settings</a>
+							<a>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="icon"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={1}
+										d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+									/>
+								</svg>
+							</a>
 						</Link>
 					</div>
 				</div>
