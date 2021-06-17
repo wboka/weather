f1838597a1d11d55ad129e3f35f60a40877462f7
Wayne Boka <wboka@users.noreply.github.com>
2021-06-17 09:09:54 -0400

    style: change to overpass

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;500;600&family=Montserrat+Alternates:wght@100&display=swap"
+						href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;400;500;600&display=swap"
 						rel="stylesheet"
 					/>
 				</Head>
