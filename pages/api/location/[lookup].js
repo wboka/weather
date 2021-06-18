ebe8732c62ebd2ff66079d668e9ea901ab635acc
wboka <boka.wayne+GitHub@gmail.com>
2021-06-18 15:30:36 -0400

    feat: change geocoding API URL

--- pages/api/location/[lookup].js
+++ pages/api/location/[lookup].js
@@ -1,9 +1,17 @@
 const baseURL =
-	"https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?benchmark=2020&format=json&address=";
+	"https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?outFields=place_Addr,address,location,type&forStorage=false&countryCode=USA&f=json&singleLine=";
 
 export default async (req, res) => {
 	const locationLookup = await fetch(`${baseURL}${req.query.lookup}`);
 	const data = await locationLookup.json();
 
-	res.status(200).json(data.result);
+	res.status(200).json({
+		addressMatches: data.candidates.map((location) => {
+			return {
+				...location,
+				matchedAddress: location.address,
+				coordinates: location.location,
+			};
+		}),
+	});
 };
