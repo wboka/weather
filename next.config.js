4c492064f3393702b735a24e463df5e29c83b3fe
Wayne Boka <boka.wayne+github@gmail.com>
2024-04-08 20:37:16 -0400

    chore: remove bad config keys

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 module.exports = withPWA({
 	directoryIndex: "index.html",
-	serverRuntimeConfig: {
-		// Will only be available on the server side
-		rollbarServerToken: process.env.ROLLBAR_SERVER_TOKEN,
-	},
-	publicRuntimeConfig: {
-		// Will be available on both server and client
-		rollbarClientToken: process.env.ROLLBAR_CLIENT_TOKEN,
-	},
+	// serverRuntimeConfig: {
+	// 	// Will only be available on the server side
+	// 	rollbarServerToken: process.env.ROLLBAR_SERVER_TOKEN,
+	// },
+	// publicRuntimeConfig: {
+	// 	// Will be available on both server and client
+	// 	rollbarClientToken: process.env.ROLLBAR_CLIENT_TOKEN,
+	// },
 	dest: "public",
 	disable: process.env.NODE_ENV === "development",
 	sass: {
